a2e33f905bb6e492d52d97375d407955d7ff38f8
Ahrumiki <tranngocminhhoangpct@gmail.com>
2024-04-15 21:31:06 +0700

    new push javafx Login manager Bill customer set-password staff-infor employee

--- src/main/java/org/example/demo/HelloApplication.java
+++ src/main/java/org/example/demo/HelloApplication.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
@@ -11,8 +12,9 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 1440, 1022);
         stage.setTitle("Hello!");
+        stage.initStyle(StageStyle.UNDECORATED);
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/org/example/demo/HelloController.java
+++ src/main/java/org/example/demo/HelloController.java
@@ -11,4 +11,5 @@ public class HelloController {
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
+
 }
