3434cafa0815c54d2cb13fb6d189ff928b686f0a
Ahrumiki <tranngocminhhoangpct@gmail.com>
2024-04-15 22:29:04 +0700

    new push javafx hello-view

--- src/main/java/org/example/demo/HelloApplication.java
+++ src/main/java/org/example/demo/HelloApplication.java
@@ -14,7 +14,7 @@ public class HelloApplication extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1440, 1022);
         stage.setTitle("Hello!");
-        stage.initStyle(StageStyle.UNDECORATED);
+        stage.setMaximized(true);
         stage.setScene(scene);
         stage.show();
     }
