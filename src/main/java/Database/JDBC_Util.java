61164d2a9199d5d1b1b9b3c980d25a0e7a1721ce
Lưu Việt Hoàng <luu186835@gmail.com>
2024-04-24 11:21:20 +0700

    Merge branch 'master' of https://github.com/hoangluu18/PBL3

---
+++ src/main/java/Database/JDBC_Util.java
@@ -0,0 +1,40 @@
+package Database;
+
+import com.sun.source.tree.BreakTree;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JDBC_Util {
+    public static Connection getConnection() throws SQLException {
+        Connection c = null;
+
+        try{
+            DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+
+            // thong so ket noi
+            String url = "jdbc:mysql://localhost:3306/pbl3";
+            String username = "root"; //sua theo may cua ban
+            String password = "Pmshoanghot1@"; //sua theo may cua ban
+
+            c = DriverManager.getConnection(url,username,password);
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+
+        return c;
+    }
+
+    public static void closeConnection(Connection c){
+        try{
+            if(c != null){
+                c.close();
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+
+        }
+    }
+}
