dfffb68faea2e827f777b8e262413c2cc1d69b43
Trần Đức Duy <doubled020504@gmail.com>
2024-04-24 16:34:02 +0700

    Remove unused comment

--- src/main/java/Controller/managerController.java
+++ src/main/java/Controller/managerController.java
@@ -32,46 +32,18 @@ public class managerController {
     @FXML
     private Button homeBtn;
 
-    @FXML
-//    public void anchorProductappear() {
-//        anchorHome.setVisible(false);
-//        anchorStaff.setVisible(false);
-//        anchorBill.setVisible(false);
-//        anchorproduct.setVisible(true);
-//
-//    }
-
+        @FXML
         public void anchorHomeappear(){
             anchorStaff.setVisible(false);
             anchorBill.setVisible(false);
             anchorHome.setVisible(true);
-//        if(1 - currentposition < 0) {
-//            TranslateTransition trans = new TranslateTransition(Duration.seconds(0.1), anchorHome);
-//            trans.setFromX(anchorHome.getWidth());
-//            trans.setToX(0);
-//            trans.play();
-//        }
-//        currentposition = 1;
         }
         @FXML
         public void anchorStaffappear(){
             anchorBill.setVisible(false);
             anchorHome.setVisible(false);
             anchorStaff.setVisible(true);
-//        if(3 - currentposition < 0) {
-//            TranslateTransition trans = new TranslateTransition(Duration.seconds(0.1), anchorHome);
-//            trans.setFromX(anchorHome.getWidth());
-//            trans.setToX(0);
-//            trans.play();
-//
-//        } else if (3 - currentposition > 0) {
-//            TranslateTransition trans = new TranslateTransition(Duration.seconds(0.1), anchorHome);
-//            trans.setFromX(-anchorHome.getWidth());
-//            trans.setToX(0);
-//            trans.play();
-//
-//        }
-//        currentposition = 3;
+
         }
         @FXML
         public void anchorBillappear(){
@@ -79,20 +51,6 @@ public class managerController {
             anchorHome.setVisible(false);
             anchorBill.setVisible(true);
 
-//        if(4 - currentposition < 0) {
-//            TranslateTransition trans = new TranslateTransition(Duration.seconds(0.1), anchorHome);
-//            trans.setFromX(anchorHome.getWidth());
-//            trans.setToX(0);
-//            trans.play();
-//
-//        } else if (4 - currentposition > 0) {
-//            TranslateTransition trans = new TranslateTransition(Duration.seconds(0.1), anchorHome);
-//            trans.setFromX(-anchorHome.getWidth());
-//            trans.setToX(0);
-//            trans.play();
-//
-//        }
-//        currentposition = 4;
         }
 
         @FXML
