fa16280894b430b4dde075aa3ba7d370210a3f18
duy <doubled020504@gmail.com>
2024-04-29 22:34:27 +0700

    Fix staff.css

--- src/main/java/View/HelloApplication.java
+++ src/main/java/View/HelloApplication.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("Staff.fxml"));
         Scene scene = new Scene(root);
         scene.getStylesheets().add(getClass().getResource("/hello-view.css").toExternalForm());
         stage.setTitle("Shop Management System");
